9ec2e48fa36e8016ceeac5d245c6ae5145348492
tenqube <kj.sa@tenqube.com>
2022-08-02 16:45:56 +0900

    key정보 수정 완료

--- app/src/main/java/com/tenqube/visualsample/MainActivity.kt
+++ app/src/main/java/com/tenqube/visualsample/MainActivity.kt
@@ -12,6 +12,8 @@ import com.tenqube.visual_third.Constants
 import com.tenqube.visual_third.VisualServiceImpl
 
 class MainActivity : AppCompatActivity() {
+    private val VISUAL_API_KEY = "" // TODO 가계부 api 키정보
+    private val RECEIPT_API_KEY = "" // TODO 영수증 api 키정보
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,16 +35,16 @@ class MainActivity : AppCompatActivity() {
         // visual service 생성
         val visualService = VisualServiceImpl(
             this,
-            "LEZQmdU1Zx8hxH1PjfT7hWTzdGOQYre58AVHNgA0", // api 키정보
-            Constants.DEV, // 레이어 정보 상용 배포시 Constants.PROD
-            "com.tenqube.visual_android_sdk" // 패키지 명
+            VISUAL_API_KEY, // TODO api 키정보
+            Constants.DEV, // TODO 레이어 정보 상용 배포시 Constants.PROD
+            applicationContext.packageName // TODO 패키지 명
         )
         // IBKMainActivity.this 값을 통해 startActivityForResult로 호출합니다.
         // IBK user 고유 아이디 정보를 추가해 주세요.
         // getVisualPath() 함수를 통해 딥링크를 통해 들어온 값을 전달합니다.
         visualService.startVisual(
             this@MainActivity, ":userUniqueId", ""
-        ) { signUpResult, msg -> }
+        ) { signUpResult, msg -> } // TODO uid 및 딥링크 패스 넣어주기
     }
 
     /**
@@ -51,18 +53,18 @@ class MainActivity : AppCompatActivity() {
     private fun startReceipt() {
         VisualServiceBuilder()
             .activity(this)
-            .apiKey("hvvDxbym1D2hYCbMnERM73rZvRopPSZh1Us4Whvq")
-            .layer(Layer.DEV) // 개발 : Layer.DEV, 상용: Layer.PROD
+            .apiKey(RECEIPT_API_KEY) // TODO 전달받은 API KEY 정보
+            .layer(Layer.DEV) // TODO 개발 : Layer.DEV, 상용: Layer.PROD
             .notification(
                 NotificationArg(
-                    R.drawable.ic_status, // 알림 small_icon 정보
-                    "ibk-channel-id", // 현재 사용 중인 알림 채널 아이디
-                    R.color.white // 색상코드
+                    R.drawable.ic_status, // TODO 알림 small_icon 정보
+                    "ibk-channel-id", // TODO 현재 사용 중인 알림 채널 아이디
+                    R.color.white // 색상코드 // TODO 알림 색상 코드
                 )
             )
             .service(Service.IBK) // IBK 고정
             .build()
-            .start(UserArg(":UID", 1987, VisualGender.MALE)) // 사용자 고유 아이디, 생년, 성별
+            .start(UserArg(":UID", 1987, VisualGender.MALE)) // TODO 사용자 고유 아이디, 생년, 성별 넣어주기
     }
 
     private fun checkPermission() {
